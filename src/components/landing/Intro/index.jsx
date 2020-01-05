7f3ce769912a2b00f07f888ce149506fa18fec2f
Vectormike <victorjonah199@gmail.com>
2020-01-05 22:17:07 +0100

    Modified landing page

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Header } from 'components/theme';
-import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/dev.svg';
-import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles';
+import { Container } from 'components/common';
+import dev from 'assets/illustrations/me.jpg';
+import { Wrapper, IntroWrapper, Details, Thumbnail, Button, Flex } from './styles';
 
 export const Intro = () => (
   <Wrapper>
     <Header />
     <IntroWrapper as={Container}>
       <Details>
-        <h1>Hi There!</h1>
-        <h4>Hi, I’m Victor. I’m a Full-Stack JavaScript Developer!</h4>
-        <Button as={AnchorLink} href="#contact">
-          Hire me
-        </Button>
+        <h4>Hi, I’m Victor Jonah.</h4>
+        <h4>Also known as Vectormike. I'm Full Stack JavaScript Developer!</h4>
+        <Flex>
+          <Button as={AnchorLink} href="#contact" rel="noopener noreferrer" target="_blank">
+            View Resume
+          </Button>
+          <Button as={AnchorLink} href="#contact">
+            Hire me
+          </Button>
+        </Flex>
       </Details>
       <Thumbnail>
         <img src={dev} alt="Hi, I’m Victor. I’m a Full-Stack JavaScript Developer!" />
--- src/components/landing/Intro/styles.js
+++ src/components/landing/Intro/styles.js
@@ -50,14 +50,53 @@ export const Details = styled.div`
   }
 `;
 
+export const Button = styled.a`
+  color: #fff;
+  text-decoration: none;
+  padding: 0.9rem 1.4rem;
+  background: #00c3ff;
+  border-radius: 0.3rem;
+  box-shadow: rgba(0, 196, 255, 0.9) 0px 13px 19px -6px;
+  transition: 0.5s;
+  @media (max-width: 380px) {
+    width: 80%;
+    margin: 0 auto;
+    text-align: center;
+  }
+  &:hover {
+    box-shadow: 0px 0px 17px 0px rgba(0, 196, 255, 0.36);
+    transition: 0.5s;
+  }
+  &:first-child {
+    margin-right: 1rem;
+    background: #f9f9f9;
+    color: #00c3ff;
+    box-shadow: rgb(185, 185, 185) 0px 13px 19px -6px;
+    &:hover {
+      box-shadow: 0px 0px 17px 0px rgba(185, 185, 185, 0.4);
+    }
+    @media (max-width: 380px) {
+      margin: 0 auto 1.5rem auto;
+    }
+  }
+`;
+
 export const Thumbnail = styled.div`
   flex: 1;
-
   @media (max-width: 960px) {
-    width: 100%;
+    width: 50%;
   }
 
   img {
-    width: 100%;
+    border-radius: 20px;
+    width: 50%;
+  }
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: 380px) {
+    flex-direction: column;
   }
 `;
