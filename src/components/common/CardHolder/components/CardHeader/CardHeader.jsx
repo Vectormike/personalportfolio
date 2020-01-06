5594dd8c38313831fca84d4fbe91e748187ff33c
Vectormike <victorjonah199@gmail.com>
2020-01-06 21:54:26 +0100

    Heading

---
+++ src/components/common/CardHolder/components/CardHeader/CardHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Wrapper, Text } from './styles';
+
+const CardHeader = () => (
+  <Wrapper>
+    <Text></Text>
+  </Wrapper>
+);
+
+export default CardHeader;
---
+++ src/components/common/CardHolder/components/CardHeader/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 1rem;
+`;
+
+export const Text = styled.h3`
+  font-size: 0.9em;
+  margin: 0;
+  font-weight: 600;
+  color: #212121;
+  @media (max-width: 680px) {
+    font-size: 1.2em;
+  }
+`;
