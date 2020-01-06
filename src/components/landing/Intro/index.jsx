96a3be8b28f1a27cde9f905c4ba2dc8d278808bc
Vectormike <victorjonah199@gmail.com>
2020-01-06 08:50:37 +0100

    New changes to intro text and svg

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Header } from 'components/theme';
 import { Container } from 'components/common';
-import dev from 'assets/illustrations/me.jpg';
+import dev from 'assets/illustrations/dev.svg';
 import { Wrapper, IntroWrapper, Details, Thumbnail, Button, Flex } from './styles';
 
 export const Intro = () => (
@@ -11,7 +11,8 @@ export const Intro = () => (
     <IntroWrapper as={Container}>
       <Details>
         <h4>Hi, I’m Victor Jonah.</h4>
-        <h4>Also known as Vectormike. I'm Full Stack JavaScript Developer!</h4>
+        <h4>Also known as Vectormike. I'm Full Stack JavaScript Developer, & Dog lover!</h4>
+        <h6>I love everything about Software Web development.</h6>
         <Flex>
           <Button as={AnchorLink} href="#contact" rel="noopener noreferrer" target="_blank">
             View Resume
--- src/components/landing/Intro/styles.js
+++ src/components/landing/Intro/styles.js
@@ -54,9 +54,9 @@ export const Button = styled.a`
   color: #fff;
   text-decoration: none;
   padding: 0.9rem 1.4rem;
-  background: #00c3ff;
+  background: #1599f5;
   border-radius: 0.3rem;
-  box-shadow: rgba(0, 196, 255, 0.9) 0px 13px 19px -6px;
+  box-shadow: #4dabba 0px 13px 19px -6px;
   transition: 0.5s;
   @media (max-width: 380px) {
     width: 80%;
@@ -64,16 +64,16 @@ export const Button = styled.a`
     text-align: center;
   }
   &:hover {
-    box-shadow: 0px 0px 17px 0px rgba(0, 196, 255, 0.36);
+    box-shadow: 0px 0px 17px 0px #4dabba;
     transition: 0.5s;
   }
   &:first-child {
     margin-right: 1rem;
-    background: #f9f9f9;
-    color: #00c3ff;
+    background: #c29a6f;
+    color: #fff;
     box-shadow: rgb(185, 185, 185) 0px 13px 19px -6px;
     &:hover {
-      box-shadow: 0px 0px 17px 0px rgba(185, 185, 185, 0.4);
+      box-shadow: 0px 0px 17px 0px #b85851;
     }
     @media (max-width: 380px) {
       margin: 0 auto 1.5rem auto;
@@ -83,15 +83,15 @@ export const Button = styled.a`
 
 export const Thumbnail = styled.div`
   flex: 1;
-  @media (max-width: 960px) {
-    width: 100%;
-    text-align: center;
-  }
-
   img {
-    border-radius: 20px;
-    width: 50%;
+    width: 100%;
+    height: 100%;
   }
+  /* Mobiles */
+  ${'' /* 0@media screen and (max-width: 425px) {
+    width: 100%;
+    height: 20px;
+  } */}
 `;
 
 export const Flex = styled.div`
