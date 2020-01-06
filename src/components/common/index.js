c2c0f822ad628b61f38df4ff73cb3ea57bf3a5a5
Vectormike <victorjonah199@gmail.com>
2020-01-06 21:51:50 +0100

    More common components

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,5 +2,6 @@ export * from './Layout';
 export * from './Container';
 export * from './Button';
 export * from './Card';
+export * from './CardHolder';
 export * from './Input';
 export * from './SEO';
