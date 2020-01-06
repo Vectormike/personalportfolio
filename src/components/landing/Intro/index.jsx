3a7fb9339815078e7c25e94d565ab84fe0ecc4c0
Vectormike <victorjonah199@gmail.com>
2020-01-06 15:09:23 +0100

    ...

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -14,7 +14,11 @@ export const Intro = () => (
         <h4>Also known as Vectormike. I'm Full Stack JavaScript Developer, & Dog lover!</h4>
         <h6>I love everything about Software Web development.</h6>
         <Flex>
-          <Button as={AnchorLink} href="#contact" rel="noopener noreferrer" target="_blank">
+          <Button
+            rel="noopener noreferrer"
+            href="https://drive.google.com/open?id=1WyXSyyfyWengLRt-yFr67vZZrc3cV5Hl"
+            target="_blank"
+          >
             View Resume
           </Button>
           <Button as={AnchorLink} href="#contact">
