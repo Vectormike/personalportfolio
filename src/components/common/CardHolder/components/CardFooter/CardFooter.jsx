3670867e2d7c8e335d2086552a45c4f7eb549677
Vectormike <victorjonah199@gmail.com>
2020-01-06 21:54:06 +0100

    Description section

---
+++ src/components/common/CardHolder/components/CardFooter/CardFooter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Wrapper, Paragraph } from './styles';
+
+const CardFooter = () => (
+  <Wrapper>
+    <Paragraph></Paragraph>
+  </Wrapper>
+);
+
+export default CardFooter;
---
+++ src/components/common/CardHolder/components/CardFooter/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 16px 24px;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 16px;
+  line-height: 1.46429em;
+  margin-bottom: 1rem;
+  color: #212121;
+`;
