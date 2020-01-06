a4c09435731c8d57167d4fedbd0ebadc93ecf7f3
Vectormike <victorjonah199@gmail.com>
2020-01-06 21:53:49 +0100

    Card body

---
+++ src/components/common/CardHolder/components/CardBody/CardBody.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import { Wrapper } from './styles';
+
+const CardBody = () => <Wrapper>{/* <Img /> */}</Wrapper>;
+
+export default CardBody;
---
+++ src/components/common/CardHolder/components/CardBody/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  overflow: visible;
+  .gatsby-image-wrapper {
+    transition: 0.3s;
+  }
+  img {
+    position: relative;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    display: block;
+    transition: 0.3s;
+  }
+`;
