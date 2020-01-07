7e67cd638f411c28d58c0d2bdaa5c8f7f32a1d4f
Vectormike <victorjonah199@gmail.com>
2020-01-07 08:16:49 +0100

    Projects

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -1,75 +1,12 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { Container, Card } from 'components/common'
-import starIcon from 'assets/icons/star.svg'
-import forkIcon from 'assets/icons/fork.svg'
-import { Wrapper, Grid, Item, Content, Stats } from './styles'
+import React from 'react';
+import { CardHolder } from 'components/common/';
+import { Wrapper, Title, Work } from './styles';
 
-export const Projects = () => {
-  const {
-    github: {
-      viewer: {
-        repositories: { edges },
-      },
-    },
-  } = useStaticQuery(
-    graphql`
-      {
-        github {
-          viewer {
-            repositories(
-              first: 8
-              orderBy: { field: STARGAZERS, direction: DESC }
-            ) {
-              edges {
-                node {
-                  id
-                  name
-                  url
-                  description
-                  stargazers {
-                    totalCount
-                  }
-                  forkCount
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-  return (
-    <Wrapper as={Container} id="projects">
-      <h2>Works</h2>
-      <Grid>
-        {edges.map(({ node }) => (
-          <Item
-            key={node.id}
-            as="a"
-            href={node.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Card>
-              <Content>
-                <h4>{node.name}</h4>
-                <p>{node.description}</p>
-              </Content>
-              <Stats>
-                <div>
-                  <img src={starIcon} alt="stars" />
-                  <span>{node.stargazers.totalCount}</span>
-                </div>
-                <div>
-                  <img src={forkIcon} alt="forks" />
-                  <span>{node.forkCount}</span>
-                </div>
-              </Stats>
-            </Card>
-          </Item>
-        ))}
-      </Grid>
-    </Wrapper>
-  )
-}
+export const Projects = () => (
+  <Wrapper>
+    <Title>Hi</Title>
+    <Work>
+      <CardHolder />
+    </Work>
+  </Wrapper>
+);
--- src/components/landing/Projects/styles.js
+++ src/components/landing/Projects/styles.js
@@ -1,61 +1,42 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  padding: 2rem 0;
+  padding-bottom: 4rem;
+  ${'' /* background-image: url(${overlayIllustration}); */}
+  background-size: contain;
+  background-position: right top;
+  background-repeat: no-repeat;
 `;
 
-export const Grid = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 8fr;
-  gap: 1.2rem 1.2rem;
+export const Title = styled.h4`
+  color: #212121;
+`;
 
+export const Work = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1.2rem;
+  align-items: center;
   @media (max-width: 960px) {
     grid-template-columns: repeat(2, 1fr);
   }
-
+  ${({ side }) =>
+    side &&
+    `
+			grid-template-columns: repeat(3, 1fr);
+			grid-gap: 3rem;
+			@media (max-width: 960px) {
+				grid-template-columns: repeat(1, 1fr);
+			}
+	`}
   @media (max-width: 680px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 4rem;
   }
-`;
-
-export const Item = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.11);
-
-  h4 {
-    color: #212121;
-  }
-
-  p {
-    color: #707070;
-  }
-`;
-
-export const Content = styled.div`
-  padding: 1rem 0;
-`;
-
-export const Stats = styled.div`
-  display: flex;
-  align-items: center;
-
-  div {
-    display: flex;
-    &:first-child {
-      margin-right: 0.5rem;
-    }
-
-    img {
-      margin: 0;
-    }
-
-    span {
-      color: #000;
-      margin-left: 0.5rem;
-    }
-  }
+  ${({ github }) =>
+    github &&
+    `
+			grid-template-columns: repeat(2, 1fr);
+			grid-template-rows: repeat(1, 8fr);
+	`};
 `;
