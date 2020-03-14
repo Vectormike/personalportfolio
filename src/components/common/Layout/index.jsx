ffd4eb103c8aadb01dbd843c17b57950dd6dcd4c
Redeemer40 <victorjonah199@gmail.com>
2020-03-14 18:40:23 +0100

    ...

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Footer } from 'components/theme'
-import { Global } from './styles'
-import './fonts.css'
+import React from 'react';
+import { Footer } from 'components/theme';
+import { Global } from './styles';
+import './fonts.css';
+
 
 export const Layout = ({ children }) => (
-	<>
-		<Global />
-		{children}
-		<Footer />
-	</>
-)
+  <>
+    <Global />
+    {children}
+    <Footer />
+  </>
+);
