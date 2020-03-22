9a981b9111b6a0953bb525019abd4574fb9a1e65
Redeemer40 <victorjonah199@gmail.com>
2020-03-22 12:40:07 +0100

    ...

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,6 +1,6 @@
 export * from './Intro';
 export * from './Skills';
 export * from './Projects';
-export * from './Contact';
+// export * from './Contact';
 export * from './Github';
 export * from './OpenSource';
