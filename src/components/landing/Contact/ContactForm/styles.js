8aed6a3a96faf38f4bbb9d8193743fa0a5e5ca47
Redeemer40 <victorjonah199@gmail.com>
2020-03-23 16:34:56 +0100

    😁

--- src/components/landing/Contact/ContactForm/styles.js
+++ src/components/landing/Contact/ContactForm/styles.js
@@ -1,18 +1,41 @@
 import styled from 'styled-components';
 
-export const Error = styled.span`
-  color: #ff4136;
-`;
-
-export const Center = styled.div`
-  text-align: left;
-
-  h4 {
-    font-weight: normal;
+export const Wrapper = styled.div`
+  padding: 6rem 0rem;
+  margin-top: 7rem;
+  color: var(--text);
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+  transition: color 0.2s ease-out;
+  @media ${({ theme }) => theme.mediaQueries.small} {
+    margin-top: 6rem;
+    padding: 4rem 0rem;
+  }
+  @media ${({ theme }) => theme.mediaQueries.smaller} {
+    padding: 3rem 0rem;
   }
 `;
 
-export const InputField = styled.div`
-  position: relative;
-  margin-bottom: 1rem;
+export const Container = styled.div`
+  max-width: 124rem;
+  z-index: 2;
+  padding: 0 4rem;
+  margin: 0 auto;
+  width: 100%;
+  @media ${props => props.theme.mediaQueries.small} {
+    padding: 0 3rem;
+  }
+  @media ${props => props.theme.mediaQueries.smaller} {
+    padding: 0 2rem;
+  }
+`;
+
+export const CopyRight = styled.p`
+  font-weight: 700;
+  margin: 0;
+  font-size: 1rem;
+  color: var(--text-highlight);
+  text-transform: uppercase;
 `;
--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Container } from 'components/common'
-import contact from 'assets/illustrations/contact.svg'
-import { Wrapper, Details, Thumbnail } from './styles'
-import ContactForm from './ContactForm'
+import React from 'react';
+import { Container } from 'components/common';
+// import contacts from 'assets/illustrations/contact.svg';
+import { Wrapper, Details, Thumbnail } from './styles';
+import { ContactForm } from './ContactForm';
 
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
     <Details>
       <ContactForm />
     </Details>
-    <Thumbnail>
-      <img src={contact} alt="I’m John and I’m a Backend & Devops engineer!" />
-    </Thumbnail>
+    {/* <Thumbnail>
+      <img src={contacts} alt="I’m John and I’m a Backend & Devops engineer!" />
+    </Thumbnail> */}
   </Wrapper>
-)
+);
