65bde31a9cc86c308f05e892f638a5cb46bf1b16
Redeemer40 <victorjonah199@gmail.com>
2020-03-23 16:33:38 +0100

    ...

---
+++ src/components/common/Header/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+// import { HeadingWrapper, Title, SubTitle } from './style';
+
+const HeadingWrapper = styled.div`
+  text-align: center;
+  margin-bottom: 7rem;
+  @media only screen and (max-width: 56.25em) {
+    margin-bottom: 5rem;
+  }
+  @media only screen and (max-width: 31.25em) {
+    margin-bottom: 4.5rem;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  margin: 0;
+  margin-bottom: 1rem;
+  color: var(--primary);
+  text-transform: uppercase;
+  font-weight: 700;
+  transition: color 0.2s ease-out;
+  @media only screen and (max-width: 37.5em) {
+    font-size: 2rem;
+  }
+  @media only screen and (max-width: 25em) {
+    font-size: 1.8rem;
+  }
+`;
+
+const SubTitle = styled.h2`
+  font-size: 2rem;
+  margin: 0;
+  color: var(--text);
+  font-weight: 400;
+  transition: color 0.2s ease-out;
+  & span {
+    color: var(--text-highlight);
+    font-style: italic;
+    font-weight: 600;
+    transition: color 0.2s ease-out;
+  }
+  @media only screen and (max-width: 37.5em) {
+    font-size: 1.9rem;
+  }
+  @media only screen and (max-width: 31.25em) {
+    font-size: 1.8rem;
+  }
+  @media only screen and (max-width: 25em) {
+    font-size: 1.7rem;
+  }
+`;
+
+export const Header = ({ title, subtitle }) => (
+  <HeadingWrapper>
+    <Title>{title}</Title>
+    <SubTitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+  </HeadingWrapper>
+);
