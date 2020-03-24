6d0bde013ba3813ac0e86c2eebd2627e4e56f9dd
Redeemer40 <victorjonah199@gmail.com>
2020-03-24 23:53:15 +0100

    ...

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { ScrollToTop } from '../scrollToTop';
 import { Global } from './styles';
 import './fonts.css';
 import theme from '../../../utils/theme';
@@ -9,6 +8,5 @@ export const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
     <Global />
     {children}
-    <ScrollToTop />
   </ThemeProvider>
 );
